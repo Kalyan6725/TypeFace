82923cad5227467b60644fb371f3a12a16fa7239
Kalyan6725 <kalyansamineni@gmail.com>
2025-09-01 09:53:16 +0530

    added backend url

--- financial-app/src/api/axios.js
+++ financial-app/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://typeface-gyzv.onrender.com", // backend URL
 });
 
 // Attach token automatically
